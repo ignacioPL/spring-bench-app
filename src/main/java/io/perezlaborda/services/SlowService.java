3d5caa09611e5e267eea314e38e486baa7be34b7
ignacioPL <perezlaborda@gmail.com>
2019-03-09 11:17:33 -0300

    add more slow service

--- src/main/java/io/perezlaborda/services/SlowService.java
+++ src/main/java/io/perezlaborda/services/SlowService.java
@@ -8,12 +8,13 @@ import java.math.BigInteger;
 public class SlowService {
 
     //private static final int N = 3000000;
+    //real slow = 90000000;
     private final BigInteger a = BigInteger.valueOf(Long.MAX_VALUE).multiply(BigInteger.valueOf(Long.MAX_VALUE));
     private final BigInteger b = BigInteger.valueOf(Long.MAX_VALUE).multiply(BigInteger.valueOf(Long.MAX_VALUE));
 
     public void loopIterator(int loopCount) {
         for(int i = 0; i < loopCount; i++){
-            BigInteger c = a.multiply(b);
+            BigInteger c = a.multiply(b).multiply(BigInteger.valueOf(i));
         }
     }
 
